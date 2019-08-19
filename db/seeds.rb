a1f8275a03772e01b0a8eaa56e1a274265c22705
Jérémie Bonal <jeremie.bonal@gmail.com>
2019-08-19 18:28:11 +0200

    Added seeding script.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+CardPayment.create!(
+  merchant_name: "CardMerchant",
+  amount_cents: 1345
+)
+
+MealVoucherCredit.create!(
+  company_name: "MealVoucherCompany",
+  amount_cents: 29000
+)
+
+RestoOrder.create!(
+  resto_name: "OrderResto",
+  amount_cents: 1350
+)
