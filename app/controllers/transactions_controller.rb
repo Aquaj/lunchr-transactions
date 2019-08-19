0823eb025d7ed87140cbcf15c4de5bb5744ebe1f
Jérémie Bonal <jeremie.bonal@gmail.com>
2019-08-19 19:03:52 +0200

    Implemented TransactionsController.

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,17 @@
 class TransactionsController < ApplicationController
   def index
+    @matching_records = scope.earlier_than(query_params[:latest_timestamp])
+                             .limit(query_params[:limit])
+    render json: @matching_records
+  end
+
+  private
+
+  def scope
+    Transaction.order(created_at: :desc)
+  end
+
+  def query_params
+    params.permit(:limit, :latest_timestamp)
   end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,7 @@
 class Transaction < ApplicationRecord
+  scope :earlier_than, -> (timestamp) { timestamp ? where("created_at < ?", timestamp) : all }
+  scope :later_than, -> (timestamp) { timestamp ? where("created_at > ?", timestamp) : all }
+
   def initialize(model)
     @model = model
   end
